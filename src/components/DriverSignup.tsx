168acdac85f0aa087c3455461b404c8b78a3e8df
Hammami Ayoub <hammami.ayoub@gmail.com>
2025-09-12 19:21:41 +0200

    Désactiver la confirmation email pour l'inscription chauffeur

--- src/components/DriverSignup.tsx
+++ src/components/DriverSignup.tsx
@@ -47,10 +47,25 @@ export const DriverSignup: React.FC<DriverSignupProps> = ({ onBack }) => {
           emailRedirectTo: undefined, // Désactiver la redirection email
           data: {
             first_name: data.firstName,
-            last_name: data.lastName
+            last_name: data.lastName,
+            email_confirm: false // Désactiver la confirmation par email
           }
         }
       });
+
+      // Alternative: Créer l'utilisateur sans confirmation email
+      /* const { data: authData, error: authError } = await supabase.auth.signUp({
+        email: data.email,
+        password: data.password,
+        options: {
+          emailRedirectTo: undefined,
+          data: {
+            first_name: data.firstName,
+            last_name: data.lastName,
+            skip_confirmation: true
+          }
+        }
+      }); */
 
       console.log('📧 Réponse Supabase Auth:', { authData, authError });
 
