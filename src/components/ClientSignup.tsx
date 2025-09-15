da1b9fd2e2fbc04959fd11d5d5cbcd82d3b7f901
Hammami Ayoub <hammami.ayoub@gmail.com>
2025-09-15 10:36:55 +0200

    Updated ClientSignup.tsx

--- src/components/ClientSignup.tsx
+++ src/components/ClientSignup.tsx
@@ -199,7 +199,7 @@ export const ClientSignup: React.FC<ClientSignupProps> = ({ onBack }) => {
               {/* VILLE — déplacé ICI sous téléphone */}
               <div className="relative">
                 <div className="absolute inset-y-0 left-0 pl-3 flex items-center pointer-events-none">
-                  
+                  <MapPin className="h-5 w-5 text-gray-400" />
                 </div>
                 <div className="pl-10">
                   <CityInput
