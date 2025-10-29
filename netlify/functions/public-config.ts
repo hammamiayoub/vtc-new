6c0d873c97ba04e3db451af43eb460f6ca93aba4
Hammami Ayoub <hammami.ayoub@gmail.com>
2025-10-29 12:55:06 +0100

    Track throught Google API

--- netlify/functions/public-config.ts
+++ netlify/functions/public-config.ts
@@ -4,6 +4,7 @@ export const handler: Handler = async () => {
   const body = {
     VITE_SUPABASE_URL: process.env.VITE_SUPABASE_URL || '',
     VITE_SUPABASE_ANON_KEY: process.env.VITE_SUPABASE_ANON_KEY || '',
+    VITE_GOOGLE_MAPS_API_KEY: process.env.VITE_GOOGLE_MAPS_API_KEY || '',
   };
   return {
     statusCode: 200,
